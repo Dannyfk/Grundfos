bf9f277447f3936ff74a1d2e082078a3fc0e447c
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-09-29 11:22:44 +0200

    searchresultlist layout start

--- index.php
+++ index.php
@@ -9,5 +9,13 @@
     <?php
 include 'header.php';
      ?>
+
+  <div class="listresult">
+    <h1 class="listresultitem1">Item1</h1>
+    <h1 class="listresultitem2">Item2</h1>
+    <h1 class="listresultitem3">Item3</h1>
+    <h1 class="listresultitem4">Item4</h1>
+  </div>
+
   </body>
 </html>
