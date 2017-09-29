0e06e6a1cbe45b20de7afec3459c038ec34fc98b
Tim Stroustrup Skov Henriksen <tim.s.h@hotmail.com>
2017-09-29 10:08:43 +0200

    header

--- header.php
+++ header.php
@@ -1,41 +1,24 @@
-<header>
-  <section id="upper-header">
-    <p>United Kingdom</p>
-    <img src="images/services.png" alt="icon" id="grid"><p>Services</p>
-    <img src="images/user.png" alt="icon" id="user"><p>United Kingdom</p>
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<meta name="description">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
 
-    <img src="images/grundfoslogo.png" alt="logo" id="logo">
-<input type="search" name="search" value="search"><img src="images/search.png" alt="icon" id="search-icon">
-  </section>
-<nav>
-  <ul class="nav main-nav">
-  								<li class="">
-  										<a href="/products.html" title="Products">Products</a>
-  									</li>
-  								<li class="">
-  										<a href="/industries-solutions.html" title="Industries &amp; solutions">Industries &amp; solutions</a>
-  									</li>
-  								<li class="">
-  										<a href="/cases.html" title="Cases">Cases</a>
-  									</li>
-  								<li class="">
-  										<a href="/service.html" title="Service &amp; support">Service &amp; support</a>
-  									</li>
-  								<li class="">
-  										<a href="/training-events.html" title="Training">Training</a>
-  									</li>
-  								<li class="">
-  										<a href="/about-us.html" title="About us">About us</a>
-  									</li>
-  								<li class="">
-  										<a href="/how-to-buy.html" title="How to buy">How to buy</a>
-  									</li>
-  								<li class=" hidden-sm hidden-md hidden-lg">
-  										Grundfos Energy check
-  									</li>
-  								<li class=" hidden-sm hidden-md hidden-lg">
-  										<a href="/grundfos-for-installers.html" title="Grundfos for Installers">Grundfos for Installers</a>
-  									</li>
-  								</ul>
-</nav>
+
+  
+</head>
+
+
+<header>
+
+
+
+
+
+
+
 </header>
