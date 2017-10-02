ed41c62494bf5849c60781bcd09e3959761775e1
Joachim Wiberg <joachimpw@gmail.com>
2017-10-02 15:28:11 +0200

    Guidelines

---
+++ guidelineSelect.php
@@ -0,0 +1,42 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+
+
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/Guideline.css">
+
+
+
+<div class="wrapper">
+<h1> Material Guidelines </h1>
+
+
+
+<section class="gridguide">
+    <div class="fade">
+        <a href="materialGuidelines.php">
+        <h2> Store </h2>
+        <img src="images/store.png">
+        <p> Hvordan i kan anvende vores materiale i jeres butik   </p>
+        </a>
+    </div>
+    
+    <div class="fade">
+        <a href="#">
+        <h2> Social Media </h2>
+        <img src="images/social.png">
+        <p> Hvordan i kan markedføre på de sociale medier </p>
+        </a>
+    </div>
+
+    <div class="fade">
+        <a href="#">
+        <h2> Webshop </h2>
+        <img src="images/webshop.png">
+        <p> Til jer med en webshop, der vil sælge online </p>
+        </a>
+    </div>
+    
+</section>
+
+</div>
