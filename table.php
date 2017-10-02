220d73b99cbcc37abbec59ea8fa47c49fb9184ff
Tim Stroustrup Skov Henriksen <tim.s.h@hotmail.com>
2017-10-02 12:25:54 +0200

    table

--- table.php
+++ table.php
@@ -1,3 +1,32 @@
+<head>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<section id="search-table-wrapper">
 <section id="table-search">
-  <img src="images"
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search2">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search3">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search4">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search5">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search6">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search7">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search8">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
+<section id="table-search9">
+  <img src="images/alpha.jpg" alt="alpha">
+</section>
 </section>
