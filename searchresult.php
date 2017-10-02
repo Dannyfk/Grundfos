8b52441a74351db79f67ba53218a7426ede4210f
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-10-02 09:02:38 +0200

    ny fil til searchresult

---
+++ searchresult.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <?php
+include 'header.php';
+     ?>
+
+  <div class="listresult">
+    <h1 class="listresultitem1">Item1</h1>
+    <h1 class="listresultitem2">Item2</h1>
+    <h1 class="listresultitem3">Item3</h1>
+    <h1 class="listresultitem4">Item4</h1>
+  </div>
+
+  </body>
+</html>
