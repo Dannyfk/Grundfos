24c52e6ea294af5ed60458198422d0b8db9b85cd
jonaskkristensen <jonaskkristensen@hotmail.dk>
2017-10-02 09:30:15 +0200

    Commit Fonts

--- mypumps.php
+++ mypumps.php
@@ -2,10 +2,34 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>MyPumps</title>
+    <link rel="stylesheet" href="css/mypumps.css">
+    <title>MyPumps | Grundfos3</title>
   </head>
   <body>
+    <figure id="myPumpsWrapper">
+      <div id="topbar">
+        MyPumps
+      </div>
+<div class="productElement">
+Element1
+</div>
 
-    
+<div class="productElement">
+Element2
+</div>
+
+<div class="productElement">
+Element3
+</div>
+
+<div class="productElement">
+Element3
+</div>
+
+<div class="productElement">
+Element3
+</div>
+
+    </figure>
   </body>
 </html>
