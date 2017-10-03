6665ac255399d8060d08fd8ed7c052ef859d00f9
Joachim Wiberg <joachimpw@gmail.com>
2017-10-03 17:08:16 +0200

    Material Guidelines

--- materialGuidelines.php
+++ materialGuidelines.php
@@ -9,23 +9,58 @@
 <div class="wrapper">
 <h1> Material Guidelines </h1>
 
+
+<section class="gridguide">
+    <div class="fade">
+        
+        <h2> Store </h2>
+        <img src="images/store.png" data-id="storeselect">
+        <p> Hvordan i kan anvende vores materiale i jeres butik   </p>
+        
+    </div>
+    
+    <div class="fade">
+        
+        <h2> Social Media </h2>
+        <img src="images/social.png" data-id="socialselect">
+        <p> Hvordan i kan markedsføre på de sociale medier </p>
+        
+    </div>
+
+    <div class="fade">
+        
+        <h2> Webshop </h2>
+        <img src="images/webshop.png" data-id="webshopselect">
+        <p> Til jer med en webshop, der vil sælge online </p>
+        
+    </div>
+    
+</section>
+
+
+
 <section class="selection">
-    <ul>
-        <li> <h2> Store </h2> <img src="images/store.png"> </li>
-        <li> <h2> Social Media </h2> <img src="images/social.png"> </li>
-        <li> <h2> Webshop </h2> <img src="images/webshop.png"> </li>
-    </ul>
+    
+         <h2> Store </h2> 
+         <h2> Social Media </h2> 
+         <h2> Webshop </h2> 
+         <img data-id="storeselect" src="images/store.png">
+         <img data-id="socialselect" src="images/social.png"> 
+         <img data-id="webshopselect" src="images/webshop.png"> 
+    
 
 </section>
 
-<section class="guides">
+<!-- <<<<<<<STORE>>>>>>>> -->
+
+<section class="guides" id="storeselect">
 
  <input id="toggle1" type="checkbox" unchecked>
-  <label for="toggle1">Images</label>
+  <label for="toggle1"> Images</label><span src="images/materialtypes/imagesicon.png"></span>
 
-    <div class="expand1">
-
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+    <div class="images1">
+        
+        <p> Lorem dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
         <img src="images/store.png">
         <img src="images/social.png">
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
@@ -35,9 +70,9 @@
     </div>
 
 <input id="toggle2" type="checkbox" unchecked>
-  <label for="toggle2">Videos</label>
+  <label for="toggle2">Selling Points</label>
 
-    <div class="expand2">
+    <div class="video1">
 
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
         <img src="images/store.png">
@@ -48,10 +83,112 @@
 
     </div>
 
-    <input id="toggle3" type="checkbox" unchecked>
+<input id="toggle3" type="checkbox" unchecked>
   <label for="toggle3">Banners</label>
 
-    <div class="expand3">
+    <div class="banners1">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+    
+
+
+</section>
+
+<!-- <<<<<Social>>>>> --> 
+
+<section class="guides" id="socialselect">
+
+ <input id="toggle4" type="checkbox" unchecked>
+  <label for="toggle4">Images</label>
+
+    <div class="images2">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+<input id="toggle5" type="checkbox" unchecked>
+  <label for="toggle5">Videos</label>
+
+    <div class="video2">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+    <input id="toggle6" type="checkbox" unchecked>
+  <label for="toggle6">Banners</label>
+
+    <div class="banners2">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+    
+
+
+</section>
+
+<!-- <<<<<Webshop>>>>>  -->
+
+<section class="guides" id="webshopselect">
+
+ <input id="toggle7" type="checkbox" unchecked>
+  <label for="toggle7">hej</label>
+
+    <div class="images3">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+<input id="toggle8" type="checkbox" unchecked>
+  <label for="toggle8">Videos</label>
+
+    <div class="video3">
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
+        <img src="images/store.png">
+        <img src="images/social.png">
+
+    </div>
+
+    <input id="toggle9" type="checkbox" unchecked>
+  <label for="toggle9">Banners</label>
+
+    <div class="banners3">
 
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tortor leo, viverra et euismod ut, vestibulum nec odio. Nulla facilisi. Vivamus in ex pretium tortor eleifend vulputate. Maecenas et venenatis nisi. Nulla ultrices vitae lorem nec euismod. Fusce scelerisque velit libero, id porta velit ultricies vitae. </p>
         <img src="images/store.png">
@@ -68,3 +205,5 @@
 </section>
 
 </div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/guidelines.js"> </script>
