efdd1c91382a1c05e69f96da0a933ca59faf75b3
Joachim Wiberg <joachimpw@gmail.com>
2017-10-06 11:52:47 +0200

    Clean up

--- search.php
+++ search.php
@@ -60,13 +60,13 @@
     </section>
     <?php include 'footer.php'; ?>
     <script src="js/search.js">
-<<<<<<< HEAD
+
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-=======
->>>>>>> 1bf0de194f1f28b123745533736a28a5aa1fab68
+
+
     </script>
   </body>
 </html>
